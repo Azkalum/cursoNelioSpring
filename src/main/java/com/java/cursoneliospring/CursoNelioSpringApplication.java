022237e04cc4cf920ac869aaaa566c99fb585b02
Azkalum <renan.132@outlook.com>
2022-05-19 14:59:03 -0300

    User entity and resource

---
+++ src/main/java/com/java/cursoneliospring/CursoNelioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.java.cursoneliospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoNelioSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/cursoneliospring/CursoNelioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.cursoneliospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
